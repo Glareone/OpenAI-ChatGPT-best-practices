5fda92c60a1f2934b5fedba98a58aeb0f8040d25
Glareone <enlarius@gmail.com>
2023-05-01 20:37:27 +0300

    visual updates, get rid of exposed token

--- ChatGPT/ChatGPT.ConsoleApp/Program.cs
+++ ChatGPT/ChatGPT.ConsoleApp/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Http.Json;
-using System.Text;
+﻿using System.Text;
 using Newtonsoft.Json;
 
 if (args.Length <= 0)
@@ -11,7 +10,7 @@ if (args.Length <= 0)
 
 HttpClient client = new();
 
-client.DefaultRequestHeaders.Add("Authorization", "Bearer <INSERT YOUR TOKEN>");
+client.DefaultRequestHeaders.Add("Authorization", "Bearer <INSERT YOUR OPENAPI TOKEN HERE>");
 
 var content = new StringContent("{\"model\": \"text-davinci-003\", \"prompt\": \"" + args[0] +
                                     "\", \"temperature\": 1, \"max_tokens\": 100}", Encoding.UTF8, "application/json");
@@ -22,12 +21,32 @@ var responseString = await response.Content.ReadAsStringAsync();
 try
 {
     var dynData = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+    string guessCommand = GuessCommand(dynData!.choices[0].text);
+    
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"> OpenAI Response: {dynData!.choices[0].text}");
+    Console.WriteLine($"> chatGPT guess at command prompt is: {guessCommand}");
     Console.ResetColor();
 }
 catch (Exception e)
 {
     Console.WriteLine($"Could not deserialized Json, error: {e.Message}");
     throw;
+}
+
+static string GuessCommand(string openAPIresponse)
+{
+    Console.WriteLine("--> GPT-3 Davinci returned");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(openAPIresponse);
+
+    var lastIndex = openAPIresponse.LastIndexOf('\n');
+
+    var guess = openAPIresponse[(lastIndex + 1)..];
+    
+    Console.ResetColor();
+    
+    TextCopy.ClipboardService.SetText(guess);
+
+    return guess;
 }
