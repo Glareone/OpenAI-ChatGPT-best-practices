f7f916dd9c55b792973a1e5943f2dcaf3464bf8a
Glareone <enlarius@gmail.com>
2023-05-01 20:21:17 +0300

    deserialization

--- ChatGPT/ChatGPT.ConsoleApp/Program.cs
+++ ChatGPT/ChatGPT.ConsoleApp/Program.cs
@@ -1,22 +1,33 @@
-﻿using System.Text;
+﻿using System.Net.Http.Json;
+using System.Text;
+using Newtonsoft.Json;
 
-if (args.Length > 0)
-{
-    HttpClient client = new();
-    
-    client.DefaultRequestHeaders.Add("Authorization", "Bearer <INSERT_YOUT_TOKEN>");
-
-    var content = new StringContent("{\"model\": \"text-davinci-003\", \"prompt\": \"" + args[0] +
-                                    "\", \"temperature\": 1, \"max_tokens\": 100}", Encoding.UTF8, "application/json");
-    var response = await client.PostAsync("https://api.openai.com/v1/completions", content);
-
-    var responseString = await response.Content.ReadAsStringAsync();
-    
-    Console.WriteLine(responseString);
-
-    Console.ReadKey();
-}
-else
+if (args.Length <= 0)
 {
     Console.WriteLine("--> You need to provide some input");
+    Console.ReadKey();
+    return;
+}
+
+HttpClient client = new();
+
+client.DefaultRequestHeaders.Add("Authorization", "Bearer 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");
+
+var content = new StringContent("{\"model\": \"text-davinci-003\", \"prompt\": \"" + args[0] +
+                                    "\", \"temperature\": 1, \"max_tokens\": 100}", Encoding.UTF8, "application/json");
+var response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+
+var responseString = await response.Content.ReadAsStringAsync();
+
+try
+{
+    var dynData = JsonConvert.DeserializeObject<dynamic>(responseString);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"> OpenAI Response: {dynData!.choices[0].text}");
+    Console.ResetColor();
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Could not deserialized Json, error: {e.Message}");
+    throw;
 }
