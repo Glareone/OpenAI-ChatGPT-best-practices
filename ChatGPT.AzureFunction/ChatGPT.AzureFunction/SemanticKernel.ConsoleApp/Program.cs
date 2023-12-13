375a1c9fd26a2b3d995b4ce0f0f7b9d5320c2375
Aleksey Kolesnikov <>
2023-12-13 23:02:45 +0200

    semantickernel for chat

--- ChatGPT.AzureFunction/ChatGPT.AzureFunction/SemanticKernel.ConsoleApp/Program.cs
+++ ChatGPT.AzureFunction/ChatGPT.AzureFunction/SemanticKernel.ConsoleApp/Program.cs
@@ -4,3 +4,7 @@ var (intentResultGpt35, intentResultGpt4) = await ShortIntentExtraction.InvokeEp
 
 Console.WriteLine(intentResultGpt35);
 Console.WriteLine(intentResultGpt4);
+
+var (gpt35content, gpt4content) = await SemanticFunctionForConversationalChat.Invoke();
+Console.WriteLine(gpt35content);
+Console.WriteLine(gpt4content);
